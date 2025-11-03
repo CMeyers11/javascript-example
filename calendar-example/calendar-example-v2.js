fc68ad7327c78246b78a590e4e631699d417b942
Chad M <meyerschad11@gmail.com>
2025-11-03 01:48:55 -0500

    Change way of finding number of days from month name to indexing an array

--- calendar-example/calendar-example-v2.js
+++ calendar-example/calendar-example-v2.js
@@ -1,21 +1,45 @@
 const select = document.querySelector("select");
 const list = document.querySelector("ul")
 const h1 = document.querySelector("h1")
+
 // Defines an obnject (key-value pairs) mapping month names to the number of days
-const daysInMonth = {
-    January: 31,
-    February: 28,
-    March: 31,
-    April: 30,
-    May: 31,
-    June: 30,
-    July: 31,
-    August: 31,
-    September: 30,
-    October: 31,
-    November: 30,
-    December: 31
-};
+
+/*Version 1
+const monthArray = [
+    ["January", 31, 1],
+    ["February", 28, 2],
+    ["March", 31, 3],
+    ["April", 30, 4],
+    ["May", 31, 5],
+    ["June", 30, 6],
+    ["July", 31, 7],
+    ["August", 31, 8],
+    ["September", 30, 9],
+    ["October", 31, 10],
+    ["November", 30, 11],
+    ["December", 31, 12],
+];
+
+const monthData = monthArray.map(([name, days, order]) => ({name, days, order}));
+*/
+
+/*Version 2*/
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+// Map Syntax: array.map(element,index*optional*)
+// Map takes an array and transforms every element into something new
+// In this case, map takes the ith element (element is defined as "name" here) and creates a 3 item array with the below data
+const monthsData = monthNames.map((name, i) => ({
+    // Sets the ith element in the array being mapped as the variable "name"
+    name,
+    //JS begins indexing at 0, so this sets the variable "days" as the 0th item in the structure daysInMonth
+    days: daysInMonth[i],
+    // JS begins indexing at 0, so this sets the variable "order" as 0+1=1
+    order: i + 1
+}));
 
 // Adds an event listener to the dropdown menu (<select> element)
 // Whenever the user changes the selected option, this function runs
@@ -30,15 +54,15 @@ select.addEventListener("change", () => {
 // Defining a function here, and thus defining what the arguments are called within this function.
 // Even though "month" has not been defined anywhere else, that doesn't matter because we are not CALLING the function here, we are DEFINING the function
 // THUS, createCalendar(choice) above is calling the createCalendar function and using "choice" as the month argument. I AM DEFINING THE SYNTAX FOR THIS FUNCTION IN THE SCRIPT BELOW!!
-function createCalendar(month) {
+function createCalendar(monthName) {
     // Conditional check to see if we have selected a valid input for the argument "month"
-    if (!daysInMonth.hasOwnProperty(month)) {
+    if (monthsData.find()) {
          "Invalid month";
        return;
     }   
     
     // Searches the object daysInMonth for the value input to the argument "month" and returns the correspoding value
-    const days = daysInMonth[month];
+    const days = daysInMonth[monthName];
 
     // Clears any existing list items (<li>) from the <ul>
     // This ensures we don't append new items on top of old ones
@@ -61,4 +85,4 @@ function createCalendar(month) {
 // Sets the dropdown to January by default when the page loads
 select.value = "January"
 // Calls the createCalendar function with January as the argument so the page starts with Jan as default
-createCalendar("January")
+createCalendar("January");
